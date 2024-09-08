0efe1f2abcdc7c2f9d67a8f167468b48692f800a
Valorie Portem <valorieportem@gmail.com>
2024-09-08 16:19:05 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("Hello, World!");
+let Name =" Julene Valorie Maitem";
+console.log('My name is'+ Name);
+
+console.log('-Using Variables-');
+let name = "Julene Valorie Maitem";
+let age = "19 ";
+console.log("Mabuhay! I am " + Name + ", " + age + "years old. ");
+
+
+console.log("-Using Numbers-");
+const V = 1 ;
+const A = 2 ;
+
+const sum = V + A;
+console.log(sum);
+
+console.log('-Using Booleans-');
+let L = true;
+let O = false;
+console.log("Programming is easy,","It's", O,".");
+console.log("I am testing my knowledge about programming,","It's", L,".");
+
+console.log('-COMPARISON OPERATORS SECTION-');
+let R = 1 ;
+let I = 2 ;
+
+let E = R > I;
+console.log("Is i more than r?"+ " " + E);
+let G = I < R;
+console.log("Is r less than i?"+ " " + G);
+let M = R === I;
+console.log("Is r equal to i?"+ " " + M);
+let N = I != R;
+console.log ("Is i not equal to r?"+ " " + N);
+ 
